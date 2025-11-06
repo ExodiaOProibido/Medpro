798b1a2320dee0984b8ce28484a4bb748ec2fbad
MykyBalb <mykaelbalbino06@gmail.com>
2025-11-05 22:28:06 -0300

    commit

---
+++ medpro/src/main/java/com/medpro/medpro/model/dto/DadosEndereco.java
@@ -0,0 +1,17 @@
+package com.medpro.medpro.model.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record DadosEndereco(
+    @NotBlank String logradouro, 
+    @NotBlank String bairro, 
+    @NotBlank @Pattern(regex = "\\d{8}") String cep, 
+    @NotBlank String cidade, 
+    @NotBlank String uf, 
+    String numero, 
+    String complemento) {
+
+    
+    
+}
