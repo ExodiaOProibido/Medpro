098457a39b0e38b048e3f88e30a5aa3e2b65945c
MykyBalb <mykaelbalbino06@gmail.com>
2025-11-07 22:38:42 -0300

    commit

--- medpro/src/main/java/com/medpro/medpro/model/entity/Medico.java
+++ medpro/src/main/java/com/medpro/medpro/model/entity/Medico.java
@@ -41,6 +41,8 @@ public class Medico {
     @Embedded
     private Endereco endereco;
 
+    private Boolean ativo;
+
     public Medico(DadosCadastroMedico dados) {
         this.nome = dados.nome();
         this.email = dados.email();
@@ -48,6 +50,7 @@ public class Medico {
         this.crm = dados.crm();
         this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
+        this.ativo = true;
     }
     
     public void atualizarInformacoes(DadosAtualizacaoMedico dados){
@@ -63,6 +66,10 @@ public class Medico {
         }
         if (dados.endereco() != null){
             this.endereco.atualizarInformacoes(dados.endereco());
+        }
+
+        public void excluir(){
+            this.ativo = false;
         }
     }
 
