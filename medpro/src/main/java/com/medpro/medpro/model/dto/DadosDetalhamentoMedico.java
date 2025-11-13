c30300760866f748332fa8c75fcaf19601b5adcb
MykyBalb <mykaelbalbino06@gmail.com>
2025-11-12 22:10:10 -0300

    commit

---
+++ medpro/src/main/java/com/medpro/medpro/model/dto/DadosDetalhamentoMedico.java
@@ -0,0 +1,28 @@
+package com.medpro.medpro.model.dto;
+
+import com.medpro.medpro.enums.Especialidade;
+import com.medpro.medpro.model.entity.Endereco; // Add this import
+import com.medpro.medpro.model.entity.Medico;
+
+public record DadosDetalhamentoMedico(
+    Long id,
+    String nome,
+    String email,
+    String telefone,
+    String crm,
+    Especialidade especialidade,
+    Endereco endereco
+) {
+    
+    public DadosDetalhamentoMedico(Medico medico) { // Fixed constructor name
+        this(
+            medico.getId(),
+            medico.getNome(),
+            medico.getEmail(),
+            medico.getTelefone(),
+            medico.getCrm(),
+            medico.getEspecialidade(),
+            medico.getEndereco()
+        );
+    }
+}
